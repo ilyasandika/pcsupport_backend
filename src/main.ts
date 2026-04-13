850c576c145dba2dca1238e219b760766fe41775
Ilyas Andika <andikailyas01@gmail.com>
2026-04-13 14:19:42 +0700

    feat: basic interceptor for API response

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { globalValidationPipeConfig } from './common/configs/validation-pipe.config';
 import { GlobalExceptionFilter } from './common/exceptions/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(globalValidationPipeConfig);
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
