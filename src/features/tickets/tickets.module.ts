ce92177b2749f6a83074a5884e8a905f8082b3bd
Ilyas Andika <andikailyas01@gmail.com>
2026-07-11 19:56:11 +0700

    feat: import template module

--- src/features/tickets/tickets.module.ts
+++ src/features/tickets/tickets.module.ts
@@ -4,9 +4,14 @@ import { TicketsController } from './tickets.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ticket } from './entities/ticket.entity';
 import { AssetAssignmentsModule } from '../asset_assignments/asset_assignments.module';
+import { TemplatesModule } from '../templates/templates.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket]), AssetAssignmentsModule],
+  imports: [
+    TypeOrmModule.forFeature([Ticket]),
+    AssetAssignmentsModule,
+    TemplatesModule,
+  ],
   controllers: [TicketsController],
   providers: [TicketsService],
 })
